197a2d9cebdf23f9f97fe0e1e83bf7ef6cf82d41
cranberrymuffin <aparnalovestocode@gmail.com>
2025-02-03 12:56:35 -0500

    fix prettier errors

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './style.css'
+import './style.css';
 
-const root = ReactDOM.createRoot(document.querySelector('#root'))
+const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
