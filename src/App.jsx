6756eb59c2dd3bde843973dd84aa52d608adc4eb
cranberrymuffin <aparnalovestocode@gmail.com>
2025-02-02 21:07:45 -0500

    simplify logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,40 @@
-import * as THREE from "three";
-import { Canvas, useFrame } from "@react-three/fiber";
-import { useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 const gridSize = 10;
 const voxelSize = 1;
+
+
+function isVisible(x, y, z) {
+  const R = gridSize / 2.5;
+  return x * x + y * y + z * z < R * R;
+}
 
 function generateVoxels() {
   let voxels = [];
   for (let x = -gridSize / 2; x < gridSize / 2; x++) {
     for (let y = -gridSize / 2; y < gridSize / 2; y++) {
       for (let z = -gridSize / 2; z < gridSize / 2; z++) {
-        voxels.push({ position: [x * voxelSize, y * voxelSize, z * voxelSize], visible: true });
+        if (isVisible(x, y, z)) {
+          voxels.push([x * voxelSize, y * voxelSize, z * voxelSize]);
+        }
       }
     }
   }
   return voxels;
 }
 
-function getSilhouette(x, y, z) {
-  const R = gridSize / 2.5;
-  return x * x + y * y + z * z < R * R;
-}
-
-function Voxel({ position, visible }) {
+function Voxel({ position }) {
   return (
-    visible && (
       <mesh position={position}>
         <boxGeometry args={[voxelSize, voxelSize, voxelSize]} />
         <meshStandardMaterial color="hotpink" />
       </mesh>
-    )
   );
 }
 
 function VoxelGrid() {
-  const [voxels, setVoxels] = useState(generateVoxels());
-
-  useEffect(() => {
-    setVoxels((prevVoxels) =>
-      prevVoxels.map((voxel) => ({
-        ...voxel,
-        visible: getSilhouette(...voxel.position),
-      }))
-    );
-
-  }, []);
-
-  return voxels.map((voxel, i) => <Voxel key={i} position={voxel.position} visible={voxel.visible} />);
+  return generateVoxels().map((voxel, i) => <Voxel key={i} position={voxel} />);
 }
 
 export default function App() {
@@ -56,25 +43,25 @@ export default function App() {
       <color attach="background" args={['pink']} />
       <ambientLight intensity={1} />
       <directionalLight
-            position={[10, 10, 10]}
-            intensity={1}
-            castShadow
-        />
-        <directionalLight
-            position={[-10, 10, -10]}
-            intensity={1}
-            castShadow
-        />
-        <directionalLight
-            position={[-10, 10, 10]}
-            intensity={1}
-            castShadow
-        />
-        <directionalLight
-            position={[10, 10, -10]}
-            intensity={1} // Brightness of the light
-            castShadow
-        />      <VoxelGrid />
+        position={[10, 10, 10]}
+        intensity={1}
+        castShadow
+      />
+      <directionalLight
+        position={[-10, 10, -10]}
+        intensity={1}
+        castShadow
+      />
+      <directionalLight
+        position={[-10, 10, 10]}
+        intensity={1}
+        castShadow
+      />
+      <directionalLight
+        position={[10, 10, -10]}
+        intensity={1} // Brightness of the light
+        castShadow
+      />      <VoxelGrid />
       <OrbitControls />
     </Canvas>
   );
